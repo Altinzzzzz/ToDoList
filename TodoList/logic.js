892a5760f3dc808fa77684c1d6045d6199a3331b
Altin <107879235+Altinzzzzz@users.noreply.github.com>
2023-03-24 16:02:09 +0100

    Added more functionality
    Still need to fix the delete button, also when I click on task it doesn't work.

--- TodoList/logic.js
+++ TodoList/logic.js
@@ -1,25 +1,29 @@
-let projects = document.querySelectorAll('.projects');
-let generalLoc = document.querySelector('.aside');
-let listOfProjects = ['Start'];
+let projects = document.querySelector('.projects');
+let allTasks = document.querySelector('#allTasks'); 
+let mainDisplay = document.querySelector('.taskContainer');
+let taskBtn = document.querySelector('#taskBtn');
+let taskForm = document.querySelector('#taskForm');
+let confirmAddTask = document.querySelector('#confirmTask');
+let cancelAddTask = document.querySelector('#cancelTask');
+let listItems;
+let listOfProjects = [];
+let activeProject;
+let allTasksContainer = [];
 let numberOfProjects = 0;
 let numberOfTasks = 0;
 
-projects.forEach(project => {
-    project.addEventListener('click', addChangeAttr(project));
-})
-
-function addChangeAttr(project){
-    for(let child of project.children){
-        console.log(child);
-        let attr = child.style.display == 'none' ? 'block' : 'none';
-        child.style.display = attr;
-    }
-}
-
 function Project(name){
-    this.id = ++numberOfProjects;
+    this.id = numberOfProjects++;
     this.name = name;
     this.taskContainer = [];
+}
+
+function Task(projectID, title, description, dueDate){
+    this.projectID = projectID;
+    this.id = ++numberOfTasks;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
 }
 
 function createProject(name){
@@ -27,46 +31,141 @@ function createProject(name){
 }
 
 function addToList(newProject){
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('projects');
-    newDiv.id = `Project${newProject.id}`;
+    const newLI = document.createElement('li');
+    newLI.id = `Project${newProject.id}`;
     let divContent = document.createTextNode(newProject.name);
-    newDiv.appendChild(divContent);
-    newDiv.addEventListener('click', addChangeAttr(newDiv));
+    newLI.appendChild(divContent);
+    newLI.addEventListener('click', () => {
+        changeMainDisplay(newProject);
+        changeAttr(newLI.id);
+    });
 
-    let location = generalLoc.lastChild;
-    location.parentNode.insertBefore(newDiv, location.nextSibling);
+    let location = projects.lastChild;
+    location.parentNode.insertBefore(newLI, location.nextSibling);
     listOfProjects[listOfProjects.length] = newProject;
 }
 
-function Task(projectID, title, description, dueDate, priority){
-    this.projectID = projectID;
-    this.id = ++numberOfTasks;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-}
-
-function createTask(projectID, title, description, dueDate, priority){
-    addToProjects(new Task(projectID, title, description, dueDate, priority));
-}
-
-function addToProjects(newTask){
-    
-    const newDiv = document.createElement('div');
-    newDiv.classList.add(`project${newTask.projectID}`);
-    newDiv.id = `task${newTask.id}`;
-    let divContent = document.createTextNode(newTask.title);
-    console.log(divContent);
-    newDiv.append(divContent);
-
-    for(let i = 1; i <= listOfProjects.length; i++){
-        if(newTask.projectID == listOfProjects[i].id){
-            document.querySelector(`#project${newTask.projectID}`).appendChild(newDiv);
-            listOfProjects[newTask.projectID].taskContainer[taskContainer.length] = newTask;
-            break;
+function createTask(projectID, title, description, dueDate){
+    let task = new Task(projectID, title, description, dueDate);
+    for(let i = 0; i < listOfProjects.length; i++){
+        if(listOfProjects[i].id == task.projectID){
+            listOfProjects[i].taskContainer.push(task);
+            allTasksContainer.push(task);
+            return;
         }
     }
+
+    numberOfTasks--;
+    return false;
 }
 
+function changeAttr(itemID){
+    listItems = projects.querySelectorAll('li');
+    for(let element of listItems){
+        if(element.classList.contains('active')){
+            element.classList.remove('active');
+            break;
+        } else if (element.id == itemID){
+            element.classList.add('active');
+        }
+    }
+    activeProject = Project;
+}
+
+function changeMainDisplay(Project){
+    let previousSib = mainDisplay.previousElementSibling;
+    previousSib.firstElementChild.style.display = 'block';
+    previousSib.previousElementSibling.textContent = Project.name;
+    mainDisplay.textContent = '';
+
+    // if(Project.taskContainer.length == 0){
+    //     let noTask = document.createElement('div');
+    //     noTask.classList.add('noTask');
+    //     noTask.textContent = 'This project has no tasks';
+    //     mainDisplay.appendChild(noTask);
+    // } else {
+        for(let i = 0; i < Project.taskContainer.length; i++){
+            addToDisplay(Project.taskContainer[i]);
+        }
+}
+
+function addToDisplay(task){
+    let newDiv = document.createElement('div');
+    let leftSideTask = document.createElement('div');
+    let rightSideTask = document.createElement('div');
+    let favBtn = document.createElement('button');
+    let delBtn = document.createElement('button');
+    let name = document.createElement('h1');
+    let theDate = document.createElement('h1'); 
+    newDiv.classList.add('task');
+    leftSideTask.classList.add('leftSideTask');
+    rightSideTask.classList.add('rightSideTask');
+    favBtn.className = 'btn btn-warning';
+    delBtn.className = 'btn btn-danger';
+    favBtn.type = 'button';
+    delBtn.type = 'button';
+    newDiv.id = task.id;
+
+    favBtn.textContent = 'FAV';
+    delBtn.textContent = 'DEL';
+    name.textContent = task.title;
+    theDate.textContent = task.dueDate;
+
+    leftSideTask.appendChild(favBtn);
+    leftSideTask.appendChild(name);
+    rightSideTask.appendChild(theDate);
+    rightSideTask.appendChild(delBtn);
+
+    newDiv.appendChild(leftSideTask);
+    newDiv.appendChild(rightSideTask);
+    mainDisplay.appendChild(newDiv);
+}
+
+function hideForm(){
+    taskForm.style.display = 'none';
+}
+
+function showForm(){
+    taskForm.style.display = 'flex';
+}
+
+taskBtn.addEventListener('click', showForm);
+cancelAddTask.addEventListener('click', hideForm);
+confirmAddTask.addEventListener('click', function(){
+    let titleInput = document.querySelector('#title');
+    let descriptionInput = document.querySelector('#description');
+    let dateinput = document.querySelector('#dueDate');
+
+    for(let i = 0; i < listOfProjects.length; i++){
+        if(activeProject == listOfProjects[i]){
+            createTask(i, titleInput, descriptionInput, dateinput);
+            hideForm();
+        }
+    }
+});
+
+allTasks.addEventListener('click', () => {
+    clearDisplay();
+
+    if(allTasksContainer.length == 0){
+        let noTask = document.createElement('div');
+        noTask.classList.add('noTask');
+        noTask.textContent = 'There are no tasks. Create or pick a project to create Tasks.';
+        mainDisplay.appendChild(noTask);
+    } else {
+        for(let i = 0; i < allTasksContainer.length; i++){
+            addToDisplay(allTasksContainer[i]);
+        }
+    }
+})
+
+function clearDisplay(){
+    let previousSib = mainDisplay.previousElementSibling;
+    previousSib.firstElementChild.style.display = 'none';
+    previousSib.previousElementSibling.textContent = 'All Tasks';
+    mainDisplay.textContent = '';
+}
+
+createProject('altini');
+document.querySelector('#Project0').style.marginTop = '35px';
+//allTasks.click();
